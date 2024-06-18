aa08438ac057585018a73bd9259bd370d8bb56e5
ditinagrawal <ditin.agrawal05@gmail.com>
2024-06-18 21:53:45 +0530

    update github link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <main className="relative grid grid-cols-1 slg:grid-cols-2 gap-12 px-4 py-12 sm:py-16 sm:px-8 md:px-10 slg:p-16 lg:p-24">
       <div className="col-span-full w-full flex flex-col items-center justify-center space-y-4 mb-4 text-center">
-        <Link href={"https://github.com/ditinagrawal"} target="_blank" className="mb-4 group">
+        <Link href={"https://github.com/ditinagrawal/x-bio-generator"} target="_blank" className="mb-4 group">
           <AnimatedGradientText className="px-6 py-2 rounded-full">
             <Star className="w-5 h-5 fill-yellow-300 text-yellow-400" />
             <hr className="mx-2 h-4 w-[1px] bg-gray-300" />
