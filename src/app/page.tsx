f61353cee34ff0f8612286db2041e698c43745c4
Ditin Agrawal <113216643+ditinagrawal@users.noreply.github.com>
2024-07-09 23:45:27 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
             <ChevronRight className="w-4 h-4 transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
           </AnimatedGradientText>
         </Link>
+        <a href="https://www.producthunt.com/posts/x-bio-generator?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-x&#0045;bio&#0045;generator" target="_blank"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=469889&theme=light" alt="X&#0032;Bio&#0032;Generator - Craft&#0032;the&#0032;perfect&#0032;X&#0032;bio&#0032;in&#0032;seconds&#0033; | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
         <h1 className="uppercase font-extrabold text-7xl text-center w-full lg:w-[90%]">craft the perfect x bio in seconds!</h1>
         <p className="text-lg text-gray-600">Just a prompt, and we'll generate a custom bio for you.</p>
       </div>
